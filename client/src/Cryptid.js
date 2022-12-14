9a7e0aa4525b4e151048b8f4b5e1c203a83bb76b
Andrew Walters <andrewjpwalters@gmail.com>
2022-12-13 23:37:45 -0600

    Add initial Submit Sighting form code

--- client/src/Cryptid.js
+++ client/src/Cryptid.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Cryptid() {
+function Cryptid({ onAddPost }) {
 
     const [formData, setFormData] = useState({
         user_id: "",
@@ -9,10 +9,57 @@ function Cryptid() {
         comment: ""
     })
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:3000/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                onAddPost(data);
+                setFormData({
+                    user_id: "",
+                    cryptid_id: "",
+                    location_id: "",
+                    comment: ""
+                })
+                console.log("Success", data);
+            })
+            .catch((error) => {
+                console.error("Error", error);
+            })
+    }
+
+    function handleChange(event) {
+        const key = event.target.id
+        const value = event.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+    }
+
     return (
-        <h1>Hello from Cryptid</h1>
-
-
+        <>
+            <h1>Hello from Cryptid</h1>
+            <h3>Submit Sighting</h3>
+            <form onSubmit={handleSubmit}>
+                <select />
+                <select />
+                <input
+                    type="text"
+                    id="comment"
+                    value={formData.comment}
+                    onChange={handleChange}
+                    placeholder="Comment..."
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </>
     )
 }
 
