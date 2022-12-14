fc3c04a8e88d8a87b0494f0d8f2ea856ce4bedc9
Andrew Walters <andrewjpwalters@gmail.com>
2022-12-13 23:00:36 -0600

    import useState to Cryptid component and define formData for useState

--- client/src/Cryptid.js
+++ client/src/Cryptid.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 function Cryptid() {
+
+    const [formData, setFormData] = useState({
+        user_id: "",
+        cryptid_id: "",
+        location_id: "",
+        comment: ""
+    })
+
     return (
         <h1>Hello from Cryptid</h1>
+
+
     )
 }
 
