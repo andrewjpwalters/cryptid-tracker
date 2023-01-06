c4613fb7971cfd3d4ad61ef8b969338a509271f0
Andrew Walters <andrewjpwalters@gmail.com>
2023-01-05 22:42:46 -0600

    add routes to posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+    def index
+        render json: Post.all
+    end
+
+    def create
+        post = @current_user.posts.create!(cryptid_params)
+        render json: post, status: created
+    end
+
+    private
+
+    def post_params
+        params.permit(:comment, :cryptid_id, :location_id)
+    end
 end
